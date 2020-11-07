29669732ee50cd012613d2f743f01aa3bdbef0e7
Satish Kumar Anbalagan <anbalagan.s@northeastern.edu>
2020-11-06 19:35:13 -0500

    read, write function declaration & implementation

--- dbserver.c
+++ dbserver.c
@@ -3,7 +3,7 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <netinet/in.h>
-
+#include <fcntl.h>
 #include <string.h>
 
 // type definition
@@ -24,6 +24,8 @@ int listener(void);
 int handle_work(int sock_fd);
 int queue_work(int sock_fd);
 int get_work();
+int read_data(char* filename, char* buf);
+int write_data(char* filename, char* buf, int len);
 
 // function definition
 
@@ -46,6 +48,23 @@ int listener() {
     return 0;
 }
 
+int read_data(char* filename, char* buf) {
+    int fd = open(filename, O_RDONLY);
+    int size = read(fd, buf, sizeof(buf));
+    printf("size %d\n", size);
+    close(fd);
+    return size;
+}
+
+int write_data(char* filename, char* buf, int len) {
+    int fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, 0777);
+    if (fd < 0)
+        perror("can't open"), exit(0);
+    int size = write(fd, buf, len);
+    close(fd);
+    return size;
+}
+
 void display_stats() {
     printf("Number of objects in the table: %d\n", stats->table_items);
     printf("Number of read requests received: %d\n", stats->no_read_request);
@@ -57,6 +76,7 @@ void display_stats() {
 }
 
 int main() {
+    system("rm -f /tmp/data.*");
     char line[128]; /* or whatever */
     stats = (stats_t*) malloc(sizeof(stats_t));
     while (fgets(line, sizeof(line), stdin) != NULL) {
